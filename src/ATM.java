a036e40f00d766cc7dc4c4d79d23ed78dba82a19
Konnie Duan <kddypublic84@yahoo.com>
2022-08-30 14:10:48 -0700

    wrote two open account methods and initialized everything

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,14 @@
-
+import java.util.*;
 public class ATM {
-
+	private HashMap <Long, Double> bankAccounts;
+	public ATM(){
+		bankAccounts=new HashMap <Long,Double>();
+	}
+	public void openAccount (long accountNumber) {
+		bankAccounts.put (accountNumber, 0.0);
+	}
+	public void openAccount (long accountNumber, double initialDeposit) {
+		bankAccounts.put (accountNumber, initialDeposit);
+	}
+	
 }
